78653e465013dc412a9bd9e9b0765978db392ccc
nicolas roldan <nicorol_95@hotmail.com>
2026-05-29 15:58:46 -0300

    feat: Corrección de modelos y constructor

--- SistemaTorneo/TorneosFutbol.API/Models/Equipo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TorneosFutbol.API.Models;
-public class Equipo
-{
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public string Nombre { get; set; } = string.Empty;
-    public string LogoUrl { get; set; } = string.Empty;
-}
